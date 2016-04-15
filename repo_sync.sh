a713be1476fcb3ef27f3b890b0bbb953534679d2
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-04-15 02:42:43 +0200

    Fixed bug: too much depth in prebuilts

--- repo_sync.sh
+++ repo_sync.sh
@@ -20,14 +20,14 @@ if [ -d ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64 ]
 	then
 		ln -s ~/android/Ryther/toolchains/UBERTC/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
 	else
-		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64/
+		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/
 		ln -s ~/android/Ryther/toolchains/UBERTC/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
 fi
 if [ -d ~/android/PureNexus/prebuilts/gcc/linux-x86/arm ]
 	then
 		ln -s ~/android/Ryther/toolchains/UBERTC/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
 	else
-		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/arm/
+		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/
 		ln -s ~/android/Ryther/toolchains/UBERTC/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
 fi
 
