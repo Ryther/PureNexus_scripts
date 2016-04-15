9ee2318dc7f2157cd458bd3ac8a9f90b105fe6a4
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-04-15 02:24:05 +0200

    Improved with directory detection

--- repo_sync.sh
+++ repo_sync.sh
@@ -16,7 +16,19 @@ cd ~/android/PureNexus/
 unlink ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
 unlink ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
 # Create new link
-ln -s ~/android/Ryther/toolchains/UBERTC/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
-ln -s ~/android/Ryther/toolchains/UBERTC/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
+if [ -d ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64 ]
+	then
+		ln -s ~/android/Ryther/toolchains/UBERTC/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
+	else
+		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64/
+		ln -s ~/android/Ryther/toolchains/UBERTC/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
+fi
+if [ -d ~/android/PureNexus/prebuilts/gcc/linux-x86/arm ]
+	then
+		ln -s ~/android/Ryther/toolchains/UBERTC/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
+	else
+		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/arm/
+		ln -s ~/android/Ryther/toolchains/UBERTC/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
+fi
 
 time repo sync -j4 -c -f --force-sync > >(tee ~/android/logs/stdout/PureNexus/stdout_repo_sync_sh.log) 2> >(tee ~/android/logs/stderr/PureNexus/stderr_repo_sync_sh.log >&2)
