7305c9c64cf5fb9899edea138fbdeb72228a8f14
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-04-15 12:30:48 +0200

    Added STANDARD_TOOLCHAIN_VERSION and path variables

--- repo_sync.sh
+++ repo_sync.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 # Repo sync script
 
+#Consts
+STANDARD_TOOLCHAIN_VERSION="4.9"
+ROM_NAME="PureNexus"
+BUILD_ROOT_PATH="~/android/${ROM_NAME}"
+CUSTOM_ROOT_PATH="~/android/Ryther"
+
 # Update the local_manifests
-cd ~/android/PureNexus/.repo/local_manifests/
+cd ${BUILD_ROOT_PATH}/.repo/local_manifests/
 git pull
 
 # Update vendors files
-cd ~/android/Ryther/vendor/lge/
+cd ${CUSTOM_ROOT_PATH}/vendor/lge/
 git pull
 
-cd ~/android/PureNexus/
+cd ${BUILD_ROOT_PATH}/
 
 # Reset to standard toolchain
 # Delete old link
-unlink ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
-unlink ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
+unlink ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/aarch64
+unlink ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/arm
 # Create new link
-if [ -d ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64 ]
+if [ -d ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/aarch64 ]
 	then
-		ln -s ~/android/Ryther/toolchains/UBERTC/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
+		ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/aarch64/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/aarch64
 	else
-		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/
-		ln -s ~/android/Ryther/toolchains/UBERTC/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
+		mkdir -p ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/
+		ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/aarch64/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/aarch64
 fi
-if [ -d ~/android/PureNexus/prebuilts/gcc/linux-x86/arm ]
+if [ -d ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/arm ]
 	then
-		ln -s ~/android/Ryther/toolchains/UBERTC/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
+		ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/arm/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/arm
 	else
-		mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/
-		ln -s ~/android/Ryther/toolchains/UBERTC/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
+		mkdir -p ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/
+		ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/arm/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/arm
 fi
 
-time repo sync -j4 -c -f --force-sync > >(tee ~/android/logs/stdout/PureNexus/stdout_repo_sync_sh.log) 2> >(tee ~/android/logs/stderr/PureNexus/stderr_repo_sync_sh.log >&2)
+time repo sync -j4 -c -f --force-sync > >(tee ${CUSTOM_ROOT_PATH}/logs/stdout/${ROM_NAME}/stdout_repo_sync_sh.log) 2> >(tee ${CUSTOM_ROOT_PATH}/logs/stderr/${ROM_NAME}/stderr_repo_sync_sh.log >&2)
