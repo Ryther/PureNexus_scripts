802d3bf917cf8e8393c019bec625f446529debc4
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-04-15 12:35:01 +0200

    Added path variables

--- repo_init.sh
+++ repo_init.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 # Repo init script
 
+# Consts
+REMOTE_REPO="https://github.com/PureNexusProject/manifest.git"
 BRANCH="mm"
 STANDARD_TOOLCHAIN_VERSION="4.9"
+ROM_NAME="PureNexus"
+BUILD_ROOT_PATH="~/android/${ROM_NAME}"
+CUSTOM_ROOT_PATH="~/android/Ryther"
 
 # Initialize the repo
-repo init -u https://github.com/PureNexusProject/manifest.git -b ${BRANCH}
+repo init -u ${REMOTE_REPO} -b ${BRANCH}
 
 # Clone custom local_manifest
-cd ~/android/PureNexus/.repo/
+cd ${BUILD_ROOT_PATH}/.repo/
 mkdir local_manifests
 cd local_manifests
-git clone https://github.com/Ryther/PureNexus_local_manifests .
+git clone https://github.com/Ryther/${ROM_NAME}_local_manifests .
 
 # Creater vendor symlink
 # Create target path
-mkdir -p ~/android/PureNexus/vendor/
+mkdir -p ${BUILD_ROOT_PATH}/vendor/
 # Create symlinks
-ln -s ~/android/Ryther/vendor/lge/ ~/android/PureNexus/vendor/lge
+ln -s ${CUSTOM_ROOT_PATH}/vendor/lge/ ${BUILD_ROOT_PATH}/vendor/lge
 
 # Creater prebuilt standard GCC symlink
 # Create target path
-mkdir -p ~/android/PureNexus/prebuilts/gcc/linux-x86/
+mkdir -p ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/
 # Create symlinks
-ln -s ~/android/Ryther/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/aarch64/ ~/android/PureNexus/prebuilts/gcc/linux-x86/aarch64
-ln -s ~/android/Ryther/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/arm/ ~/android/PureNexus/prebuilts/gcc/linux-x86/arm
+ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/aarch64/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/aarch64
+ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/arm/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/arm
