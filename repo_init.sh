1df902e1bea0caf349b28be98ee0e0db75b04cf8
Edoardo Zanoni <Ryther@users.noreply.github.com>
2016-05-04 17:38:00 +0200

    Temp: removed symlinking

--- repo_init.sh
+++ repo_init.sh
@@ -31,21 +31,21 @@ git clone ${REMOTE_CUSTOM_REPO}/${ROM_NAME}_local_manifests .
 
 # Creater vendor symlink
 # Create target path
-echo ""
-echo "Creating vendor path..."
-mkdir -p ${BUILD_ROOT_PATH}/vendor/
+#echo ""
+#echo "Creating vendor path..."
+#mkdir -p ${BUILD_ROOT_PATH}/vendor/
 # Create symlinks
-echo ""
-echo "Creating vendor symlinks..."
-ln -s ${CUSTOM_ROOT_PATH}/vendor/lge/ ${BUILD_ROOT_PATH}/vendor/lge
+#echo ""
+#echo "Creating vendor symlinks..."
+#ln -s ${CUSTOM_ROOT_PATH}/vendor/lge/ ${BUILD_ROOT_PATH}/vendor/lge
 
 # Creater prebuilt standard GCC symlink
 # Create target path
-echo ""
-echo "Creating prebuilts gcc path..."
-mkdir -p ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/
+#echo ""
+#echo "Creating prebuilts gcc path..."
+#mkdir -p ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/
 # Create symlinks
-echo ""
-echo "Creating prebuilts gcc symlinks..."
-ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/aarch64/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/aarch64
-ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/arm/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/arm
+#echo ""
+#echo "Creating prebuilts gcc symlinks..."
+#ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/aarch64/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/aarch64
+#ln -s ${CUSTOM_ROOT_PATH}/toolchains/UBERTC/${STANDARD_TOOLCHAIN_VERSION}/arm/ ${BUILD_ROOT_PATH}/prebuilts/gcc/linux-x86/arm
